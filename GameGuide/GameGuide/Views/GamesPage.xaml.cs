6d36ccefa3fbf90b6c09c7a0040bdcc164bd6cb7
okamodgi <nadakupnada@gmail.com>
2024-06-22 09:12:43 +0300

    половина

--- GameGuide/GameGuide/Views/GamesPage.xaml.cs
+++ GameGuide/GameGuide/Views/GamesPage.xaml.cs
@@ -1,32 +1,34 @@
 ﻿using Xamarin.Forms;
+using System.Linq;
 
-public partial class GamesPage : ContentPage
+namespace GameGuide.Views
 {
-    GamesViewModel viewModel;
-
-    public GamesPage()
+    public partial class GamesPage : ContentPage
     {
-        InitializeComponent();
+        GamesViewModel viewModel;
+        public GamesPage()
+        {
+            InitializeComponent();
 
-        BindingContext = viewModel = new GamesViewModel();
-    }
+            BindingContext = viewModel = new GamesViewModel();
+        }
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        if (viewModel.Games.Count == 0)
-            viewModel.LoadGamesCommand.Execute(null);
-    }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (viewModel.Games.Count == 0)
+                viewModel.LoadGamesCommand.Execute(null);
+        }
 
-    async void OnGameSelected(object sender, SelectionChangedEventArgs args)
-    {
-        var game = args.CurrentSelection.FirstOrDefault() as Game;
-        if (game == null)
-            return;
+        async void OnGameSelected(object sender, SelectionChangedEventArgs args)
+        {
+            var game = args.CurrentSelection.FirstOrDefault() as Game;
+            if (game == null)
+                return;
 
-        await Navigation.PushAsync(new SectionsPage(game.Id));
+            await Navigation.PushAsync(new SectionsPage(game.Id));
 
-        // Manually deselect item.
-        ((CollectionView)sender).SelectedItem = null;
+            ((CollectionView)sender).SelectedItem = null;
+        }
     }
 }
